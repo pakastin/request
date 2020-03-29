ed3a0bee5b23041b731a07f47e0ab60a6c2438d4
Juha Lindstedt <juha@pakastin.fi>
2020-03-29 09:09:37 +0300

    Refactor

--- index.js
+++ index.js
@@ -1,105 +1,36 @@
+/* global XMLHttpRequest */
 
-export function post (url, data, cb, pcb) {
-  var request = new XMLHttpRequest();
+export function get (url, cb) {
+  return createRequest('GET', url, null, cb);
+}
 
-  request.open('POST', url, true);
+export function post (url, data, cb) {
+  return createRequest('POST', url, data, cb);
+}
 
-  request.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
-  request.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+function createRequest (method, url, data, cb) {
+  const req = new XMLHttpRequest();
 
-  request.onload = function () {
-    if (request.status >= 200 && request.status < 400) {
-      cb && cb(null, request.responseText);
-    } else {
-      cb && cb(request.statusText, request.responseText);
-    }
-  }
+  req.open(method, url, true);
 
-  request.onprogress = function (e) {
-    pcb && pcb(request.responseText);
-  }
+  req.onload = onload;
+  req.onerror = onerror;
 
-  request.onerror = function (err) {
-    cb && cb(err, request.responseText);
-  }
-
-  if (data) {
-    request.send(data);
+  if (data != null) {
+    req.send(data);
   } else {
-    request.send();
+    req.send();
   }
-  
-  return request;
-}
 
-export function get (url, cb, pcb) {
-  var request = new XMLHttpRequest();
-
-  request.open('GET', url, true);
-
-  request.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-
-  request.onload = function () {
-    if (request.status >= 200 && request.status < 400) {
-      cb && cb(null, request.responseText);
+  function onload () {
+    if (req.status === 200) {
+      cb(null, req.responseText);
     } else {
-      cb && cb(request.statusText, request.responseText);
+      cb(req.status, req.responseText);
     }
   }
 
-  request.onprogress = function (e) {
-    pcb && pcb(request.responseText);
+  function onerror (err) {
+    cb(new Error(err));
   }
-
-  request.onerror = function (err) {
-    cb && cb(err, request.responseText);
-  };
-
-  request.send();
-
-  return request;
-}
-
-
-export function jsonstream (url, cb, pcb) {
-  var buffered = 0;
-  var request = new XMLHttpRequest();
-  var results = [];
-
-  request.open('GET', url, true);
-
-  request.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-
-  request.onload = function () {
-    if (request.status >= 200 && request.status < 400) {
-      buffer(request.responseText, true);
-      cb && cb(null, results.join(','));
-    } else {
-      cb && cb(request.statusText, request.responseText);
-    }
-  };
-
-  request.onprogress = function (e) {
-    buffer(request.responseText, false);
-  }
-
-  request.onerror = function (err) {
-    cb && cb(err, request.responseText);
-  };
-
-  request.send();
-
-  function buffer (data, last) {
-    var parts = data.slice(1).split('\u2028');
-    var len = last ? parts.length : parts.length - 1;
-
-    while (buffered < len) {
-      var part = parts[buffered].slice(0, -1);
-      results.push(part);
-      pcb && pcb('[' + part + ']');
-      buffered++;
-    }
-  }
-
-  return request;
 }
